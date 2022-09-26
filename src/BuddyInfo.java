c743c68d28483a7e8cebf21775571b29ae509991
access6080 <access.6080@gmail.com>
2022-09-26 14:17:47 -0400

    Committing New files

---
+++ src/BuddyInfo.java
@@ -0,0 +1,35 @@
+public class BuddyInfo {
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this.name = "";
+        this.address = "";
+        this.phoneNumber = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+    private String name;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private String address;
+    private String phoneNumber;
+
+    public static void main(String[] args) {
+
+        BuddyInfo homer = new BuddyInfo("Homer", "1900 Capwood", "123-456-7898");
+        System.out.println("Hello " + homer.getName());
+    }
+}
